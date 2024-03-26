5496e261a1b59681dd34bcf8b2d515b2ed2bb829
WeiTsungCheng <forwindsun1359@gmail.com>
2024-03-26 19:30:42 +0800

    Initial Commit

---
+++ ColorfulPlaid/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ColorfulPlaid
+//
+//  Created by WEI-TSUNG CHENG on 2024/3/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
